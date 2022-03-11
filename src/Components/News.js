19ae2aa840c16e8df18c674f40cd4c62351a5a84
Harsh Sharma <harshsb98@gmail.com>
2022-03-11 17:36:50 +0530

    Stick NavBar & Refactoring

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,95 +1,105 @@
-import React, { useEffect,useState } from 'react'
-import NewsItem from './NewsItem'
-import Spinner from './Spinner';
-import PropTypes from 'prop-types'
+import React, { useEffect, useState } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-
-
-
-
 const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
 
-    const [articles, setArticles] = useState([])
-    const [loading, setLoading] = useState(true)
-    const [page, setPage] = useState(1)
-    const [totalResults, setTotalResults]= useState(0)
-    //document.title=`${capitalizF(props.category)} - News Surfer`;
+  const capitalizF = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
 
-    const capitalizF = (string) =>{
-      return string.charAt(0).toUpperCase()+string.slice(1);
-    }
+  const updateNews = async () => {
+    props.setProgress(25);
+    let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    setLoading(true);
+    let data = await fetch(url);
+    props.setProgress(50);
+    let parsedData = await data.json();
+    props.setProgress(75);
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
+    props.setProgress(100);
+  };
 
-    const updateNews = async ()=>{
-      props.setProgress(25);
-      let url= `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
-      setLoading(true)
-      let data= await fetch(url);
-      props.setProgress(50);
-      let parsedData= await data.json();
-      props.setProgress(75);
-      setArticles(parsedData.articles);
-      setTotalResults(parsedData.totalResults);
-      setLoading(false);
-      props.setProgress(100);
-    }
+  useEffect(() => {
+    document.title = `${capitalizF(props.category)} - News Surfer`;
+    updateNews();
+    // eslint-disable-next-line
+  }, []);
 
-    useEffect(()=>{
-        updateNews();
-        // eslint-disable-next-line
-      },[])
+  const fetchMoreData = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=${
+      props.country
+    }&category=${props.category}&apiKey=${props.apiKey}&page=${
+      page + 1
+    }&pageSize=${props.pageSize}`;
+    setPage(page + 1);
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    setArticles(articles.concat(parsedData.articles));
+    setTotalResults(parsedData.totalResults);
+  };
 
-    const fetchMoreData = async () => {
-      setPage(page+1)
-    let url= `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
-      let data= await fetch(url);
-      let parsedData= await data.json();
-      setArticles(articles.concat(parsedData.articles));
-      setTotalResults(parsedData.totalResults);
-    };
-    
-
-    return (
-  
-      <>
-        <h1 className='text-center' style={{margin:'35px 0px'}}>News Surfer - Top { 
-        capitalizF(props.category)} Headlines
-        </h1>
-        {loading && <Spinner />}
-        <InfiniteScroll
-          dataLength={articles.length}
-          next={fetchMoreData}
-          hasMore={articles.length!==totalResults}
-          loader={<Spinner />}>
-            <div className="container">
-
-        <div className='row'>
-        {(articles).map(
-            (element) => {
-            return <div className="col-md-4" key={element.url}>
-            <NewsItem  title={element.title?element.title:""} source={element.source.name} description={element.description?element.description:""} imageUrl={!element.urlToImage?"https://www.thermaxglobal.com/wp-content/uploads/2020/05/image-not-found-300x169.jpg":element.urlToImage} newsUrl={element.url} author={element.author?element.author:"Unknown"} date={element.publishedAt}/>
-            </div>
-          }
-        )}
+  return (
+    <>
+      <h1
+        className="text-center"
+        style={{ margin: "35px 0px", marginTop: "90px" }}
+      >
+        News Surfer - Top {capitalizF(props.category)} Headlines
+      </h1>
+      {loading && <Spinner />}
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner />}
+      >
+        <div className="container">
+          <div className="row">
+            {articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title : ""}
+                    source={element.source.name}
+                    description={element.description ? element.description : ""}
+                    imageUrl={
+                      !element.urlToImage
+                        ? "https://www.thermaxglobal.com/wp-content/uploads/2020/05/image-not-found-300x169.jpg"
+                        : element.urlToImage
+                    }
+                    newsUrl={element.url}
+                    author={element.author ? element.author : "Unknown"}
+                    date={element.publishedAt}
+                  />
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div></InfiniteScroll>
-     
-      </>
-    )
-  
-}
+      </InfiniteScroll>
+    </>
+  );
+};
 
-News.defaultProps={
-  country:'in',
-  pageSize:6,
-  category: 'general',
-}
+News.defaultProps = {
+  country: "in",
+  pageSize: 6,
+  category: "general",
+};
 
-News.propTypes={
+News.propTypes = {
   country: PropTypes.string,
   pageSize: PropTypes.number,
   category: PropTypes.string,
-}
+};
 
-export default News
-
+export default News;
